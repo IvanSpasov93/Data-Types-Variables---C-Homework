3a5ca2113e4b46b5bfe7fba5578fe5580ddc6c45
IvanSpasov93 <northmc93@gmail.com>
2017-01-28 23:47:55 +0200

    Centuries to seconds

---
+++ 10.CenturiesToSeconds.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Write program to enter an integer number of centuries and
+//convert it to years, days, hours, minutes, seconds
+
+namespace _10.CenturesToNanoseconds
+{
+    class CenturiesToSeconds
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter centuries:");
+            int centuries = int.Parse(Console.ReadLine());
+
+            int years = centuries * 100;
+            int days = years * 365;
+            int hours = days * 24;
+            int sec = hours * 60;
+
+            Console.WriteLine(sec + " seconds.");
+
+        }
+    }
+}
