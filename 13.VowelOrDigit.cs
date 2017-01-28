d93ca54fff9edd705096ff000a251140b85f6250
IvanSpasov93 <northmc93@gmail.com>
2017-01-28 23:49:25 +0200

    Vowel or a Digit Problem

---
+++ 13.VowelOrDigit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Create a program to check if given symbol is digit, vowel or any other symbol
+
+namespace _13.VowelOrDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char symbol;
+
+            Console.Write("Input a symbol: ");
+            symbol = Convert.ToChar(Console.ReadLine());
+
+            if ((symbol == 'a') || (symbol == 'e') || (symbol == 'i') ||
+                    (symbol == 'o') || (symbol == 'u'))
+                Console.WriteLine("It's a lowercase vowel.");
+            else if ((symbol >= '0') && (symbol <= '9'))
+                Console.WriteLine("It's a digit.");
+            else
+                Console.Write("It's another symbol.");
+        }
+    }
+}
