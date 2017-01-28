e45eec9e6573fa71504cf6437c698455d37fb1da
IvanSpasov93 <northmc93@gmail.com>
2017-01-28 23:43:54 +0200

    Strings and Characters Problem

---
+++ 03.StringsAndCharacters.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Create a new C# project and create a program that assigns character and string values to variables. 
+//Be sure that each value is stored in the correct variable.
+//Finally, you need to print all variables to the console.
+
+namespace _03.StringsAndCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string string1 = "Hello SoftUni!";
+            char b = 'b';
+            char y = 'y';
+            char e = 'e';
+
+            Console.WriteLine(string1);
+            Console.WriteLine(b);
+            Console.WriteLine(y);
+            Console.WriteLine(e);
+        }
+    }
+}
