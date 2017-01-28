d0aa34c15df4495ddfaa30ed3ab08e042b884a4d
IvanSpasov93 <northmc93@gmail.com>
2017-01-28 23:50:15 +0200

    Fast Prime Checker Problem

---
+++ 15.FastPrimeChecker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Write an expression that checks if given positive integer number n (n ≤ 100)
+//is prime (i.e. it is divisible without remainder only to itself and 1).
+
+namespace _15.FastPrimeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter positive integer number <=100");
+            uint num = uint.Parse(Console.ReadLine());
+            if (num <= 1)
+            {
+                Console.WriteLine("This integer is NOT a prime number!");
+            }
+            else if (num > 1 && num <= 100) // Checks numbers in interval
+            {
+                if (num == 2 || num == 3 || num == 5 || num == 7) // Checks numbers 2, 3, 5, 7
+                {
+                    Console.WriteLine("This integer IS prime number!");
+                }
+                else
+                {
+                    if (num % 2 != 0 && num % 3 != 0 && num % 5 != 0 && num % 7 != 0) // Checks division with reminder
+                    {
+                        Console.WriteLine("This integer IS a prime number!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("This integer is NOT a prime number!");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input! Please enter an integer which is in range!");
+            }
+        }
+    }
+}
