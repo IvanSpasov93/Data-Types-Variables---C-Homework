373e5436587201695c67df4b0833a068a3cc8b92
IvanSpasov93 <northmc93@gmail.com>
2017-01-28 23:40:17 +0200

    01.IntegerNumbers Problem
    the integer number variable problem

---
+++ 01.IntegerNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Create a new C# project and create a program that assigns integer values to variables. Be sure 
+//that each value is stored in the correct variable type (try to find the most suitable variable
+//type in order to save memory). 
+//Finally, you need to print all variables to the console.
+
+class Program
+{
+    static void Main()
+    {
+        byte num1 = 128;
+        sbyte num2 = -100;
+        int num3 = 1323234;
+
+        Console.WriteLine(num1 + " " + num2 + " " + num3);
+    }
+}
+
