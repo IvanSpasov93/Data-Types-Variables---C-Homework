b9611b23d434d6fc79ac2347dfd2723744b19b4d
IvanSpasov93 <northmc93@gmail.com>
2017-01-28 23:47:32 +0200

    Reversing Characters Problem

---
+++ 09.ReverseCharacters.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Write a program to ask the user for 3 letters and print them in reversed order.
+
+namespace _09.ReverseCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = Convert.ToChar(Console.ReadLine());
+            char b = Convert.ToChar(Console.ReadLine());
+            char c = Convert.ToChar(Console.ReadLine()); //input of single char;
+
+            Console.WriteLine();
+            Console.WriteLine(c + " " + b + " " + a);
+        }
+    }
+}
